6a56647d4550d7484ede0088a48ff77f1bfd4c73
GrPugaguga <pugaguga@mail.ru>
2025-11-10 15:25:32 +0500

    init

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { EventsModule } from './events/events.module';
+import { BookingsModule } from './bookings/bookings.module';
+import { DatabaseModule } from './database/database.module';
+import { CacheModule } from './cache/cache.module';
+
+@Module({
+  imports: [EventsModule, BookingsModule, DatabaseModule, CacheModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
